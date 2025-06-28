218cb975ea7e689895759f081912df46cbdfe159
vernyuy <fonchu.e.venyuy@gmail.com>
2025-06-28 14:25:44 +0100

    add storage backend

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,7 +3,7 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'momentumStorage',
   access: (allow) => ({
-    'images': [
+    'images/*': [
       allow.authenticated.to(['read','write']),
       allow.guest.to(['read', 'write'])
     ],
