944e07a26b22c4030b716b47ead4f0d012dfd33b
ef <60782360+vernyuy@users.noreply.github.com>
2025-08-01 23:17:49 +0100

    Update resource.ts

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -83,7 +83,7 @@ export const data = defineData({
     defaultAuthorizationMode: "apiKey",
     // API Key is used for a.allow.public() rules
     apiKeyAuthorizationMode: {
-      expiresInDays: 30,
+      expiresInDays: 365,
     },
   },
 });
